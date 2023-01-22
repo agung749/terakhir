b852a1c6bc3078299ee721f62ae69f3a11b43758
agung749 <60955307+agung749@users.noreply.github.com>
2023-01-22 21:22:14 +0700

    Update Kelas.php

--- app/Models/Kelas.php
+++ app/Models/Kelas.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Kelas extends Model
 {
     use HasFactory;
-    protected $table="Kelas";
+    protected $table="kelas";
     protected $fillable=["id",'nama',"jumlah","jurusan","posisi","jumlah_terisi","angkatan"];
 }
