1c9da945aa388bad21c59eff6dffd9f21aac2aba
agung749 <60955307+agung749@users.noreply.github.com>
2023-01-22 22:34:47 +0700

    Update PendaftaranController.php

--- app/Http/Controllers/PendaftaranController.php
+++ app/Http/Controllers/PendaftaranController.php
@@ -73,6 +73,9 @@ class PendaftaranController extends Controller
     $foto_skhu="";
    $req->validate([
        'nama'=>'required',
+       'email'=>'required',
+        'no_tel'=>'required|digits_between:12,14',
+        'no_hp'=>'required|digits_between:12,14',
        'nisn'=>'required|unique:siswa',
        'nik'=>'required|digits_between:16,17',
        'foto'=>'nullable|max:300000|mimes:jpg,png,pdf',
