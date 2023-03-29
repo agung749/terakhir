3b8440ff0072a3e3cb9033be7323b784b77e574d
agung749 <60955307+agung749@users.noreply.github.com>
2023-03-29 09:44:00 +0700

    V
    V

--- app/Exports/Pendaftaran.php
+++ app/Exports/Pendaftaran.php
@@ -2,7 +2,7 @@
 
 namespace App\Exports;
 
-use App\Models\siswa;
+use App\Models\Siswa;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithHeadings;
