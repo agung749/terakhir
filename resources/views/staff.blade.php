1aa57268e954e356d95aac6c1e10801870b893b3
agung749 <60955307+agung749@users.noreply.github.com>
2023-04-20 11:50:29 +0700

    Update staff.blade.php

--- resources/views/staff.blade.php
+++ resources/views/staff.blade.php
@@ -43,7 +43,27 @@
                             <li><a href="/staff/kategori/Struktural">Struktural<span class="badge pull-right">{{ $kategori[2] }}</span></a></li>
                             <li><a href="/staff/kategori/Caraka">Caraka<span class="badge pull-right">{{ $kategori[3] }}</span></a></li>
                         </div>	
+                        
 					</div>
+                  <div class="row">
+                    <div class="col-md-12">
+                        <h4>Kritik Dan Saran</h4>
+                    <form  action="/saran" method="POST" >
+					@csrf
+						<input type="text" class="form-control" id="name" name="nama" placeholder="Your Full Name" style="margin-bottom: 3%" >
+						<input type="email" class="form-control" id="email" name="email" placeholder="Your E-mail" style="margin-bottom: 3%" >
+						<select name="kategori" id="kategori" class="form-control " style="margin-bottom: 3%" >
+						<option value="fasilitas">Fasilitas</option>0
+						<option value="Guru">Guru</option>
+						<option value="Keuangan">Keuangan</option>
+						</select>
+						<textarea id="mssg" name="saran" placeholder="Your Message" class="form-control" rows="4"></textarea>
+						<button class="btn btn-main btn-xs" type="submit" id="send"><i class="fa fa-paper-plane "></i>
+							Kirim</button>
+					</form>
+					<div id="result-message" role="alert"></div>
+                    </div>
+                  </div>
 				</div>
 					@if(count($cari)!=0)
                     <div class="col-md-8 ">
@@ -76,14 +96,16 @@
                             @endif
                             @endforeach
                         </div>
+                        {{$cari->links()}}
                         </div>
                     @else
                     <div class="col-md-8">
                         <h1 style="margin-top:35%; text-align:center">Data Kosong</h1>
                     </div> 
                     @endif                  
-                
-			</div>
+                   
+			        </div>
+            
 		<DIV>
       
         </DIV>	
