c8698814c42027ee3e7c021b947a9268921795e9
agung749 <agungciccone749@gmai.com>
2023-05-14 16:06:11 +0700

    a'a

--- app/Http/Controllers/kelolaUser.php
+++ app/Http/Controllers/kelolaUser.php
@@ -15,15 +15,18 @@ class kelolaUser extends Controller
   }
   public function ubahPSWD(Request $req)
   {
+  
     $user = User::where('id', Auth::user()->id);
     if (Auth::attempt(['email' => Auth::user()->email, 'password' => $req->password_lama])) {
       $req->validate([
         'password' => "min:8|confirmed"
       ]);
-      $user->update(['password' => Hash::make($req->password)]);
-      return redirect('/user/ubah')->with(['success' => "KATA SANDI BERHASIL DIUBAH "]);
+   
+      $user->update(['password' => Hash::make($req->password_baru)]);
+      return redirect('/user/ubah',['success' => "KATA SANDI BERHASIL DIUBAH "]);
     }
     else{
+      dd($req->password);
       return redirect('/user/ubah')->withErrors(['error' => "KATA SANDI SALAH "]);
     }
   }
