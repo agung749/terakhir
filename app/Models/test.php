1bf8b90bc9cff6c347330c90796984f465aef82b
agung749 <agungciccone749@gmail.com>
2023-07-08 23:18:44 +0700

    zzz

--- app/Models/test.php
+++ app/Models/test.php
@@ -8,7 +8,7 @@ class test extends Model
 {
     use HasFactory;
     protected $table="test";
-    protected $fillable=["nilai_wawancara","nilai_btq","nilai_diagnostik",'nilai_total',"siswa_id"];
+    protected $fillable=["nilai_wawancara","nilai_btq","nilai_diagnostik",'nilai_total',"siswa_id","catatan"];
     public function siswa()  {
         return $this->hasOne("\App\Models\Siswa","id","siswa_id");
     }
